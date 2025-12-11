031b2da21e932264a19c86c5bfa93c095bbf5050
Hossam <hosssam4009@gmail.com>
2025-12-11 13:58:55 +0200

    hossam

--- TrainigSectorWebSite/Controllers/QualityController.cs
+++ TrainigSectorWebSite/Controllers/QualityController.cs
@@ -1,21 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
-
+using TrainigSectorDataEntry.Interface;
+using TrainigSectorDataEntry.Logging;
+using TrainigSectorDataEntry.Models;
+using TrainigSectorDataEntry.ViewModel;
 namespace TrainigSectorWebSite.Controllers
 {
-    public class QualityController : Controller
+    public class QualityController :  BaseController
     {
         IStringLocalizer<SharedResource> _localizer;
-        public QualityController(IStringLocalizer<SharedResource> localizer)
+        private readonly IGenericService<QualityCertificate> _QualityCertificate;
+       
+
+        private readonly IGenericService<TrainingSector> _trainingSectorService;
+
+        private readonly IMapper _mapper;
+        private readonly ILoggerRepository _logger;
+
+
+        public QualityController(IStringLocalizer<SharedResource> localizer, IGenericService<QualityCertificate> QualityCertificate, IGenericService<TrainingSector> trainingSectorService, IMapper mapper)
         {
+            _QualityCertificate = QualityCertificate;
+            
+            _trainingSectorService = trainingSectorService;
             _localizer = localizer;
+            _mapper = mapper;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int ID)
         {
-            ViewData["Breadcrumb_MapPath"] = "معامل";
-            ViewData["Breadcrumb_PageName"] = "Quality";
-            ViewData["Breadcrumb_ActivePage"] = "معامل هندسية";
-            return View();
+            SetBreadcrumb(
+            mapPath: _localizer["MainPage"],
+            pageName: _localizer["TrainingHistory"],
+            activePage: _localizer["TrainingHistory"]
+);
+            var result = await _QualityCertificate.GetAllAsyncByEducationalFacilitiesId(false, ID);
+
+
+
+
+
+
+
+
+            var viewModelList = _mapper.Map<List<QualityCertificateVM>>(result);
+            return View(viewModelList);
+
+
+
+
         }
     }
 }
