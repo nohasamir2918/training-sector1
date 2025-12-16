33d1decd7cbac3618f25c1714872f7852a74a2bb
Hossam <hosssam4009@gmail.com>
2025-12-16 08:49:21 +0200

    hossam

--- TrainigSectorDataEntry/ViewModel/ComplaintsAndSuggestionVM.cs
+++ TrainigSectorDataEntry/ViewModel/ComplaintsAndSuggestionVM.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 using TrainigSectorDataEntry.Models;
 
 namespace TrainigSectorDataEntry.ViewModel
@@ -9,34 +11,34 @@ namespace TrainigSectorDataEntry.ViewModel
 
         public int TrainigSectorId { get; set; }
 
+        [Required(ErrorMessage = "الاسم مطلوب")]
         public string Name { get; set; } = null!;
 
+        [Required(ErrorMessage = "رقم التليفون مطلوب")]
+        [RegularExpression(@"^[0-9]+$",
+    ErrorMessage = "رقم التليفون يجب أن يحتوي على أرقام فقط")]
         public int Telephone { get; set; }
 
+        [Required(ErrorMessage = "البريد الإلكتروني مطلوب")]
+        [EmailAddress(ErrorMessage = "البريد الإلكتروني غير صحيح")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "نص الشكوى / المقترح مطلوب")]
         public string ComplaintText { get; set; } = null!;
 
-        public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "يرجى إرفاق ملف")]
+        public IFormFile UploadedFile { get; set; } = null!;
+
         [ValidateNever]
         public string FilePath { get; set; } = null!;
 
-        public bool IsActive { get; set; }
-
-        public bool? IsDeleted { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool? IsDeleted { get; set; } = false;
 
         public int? UserCreationId { get; set; }
-
         public DateOnly? UserCreationDate { get; set; }
 
-        public int? UserUpdationId { get; set; }
-
-        public DateOnly? UserUpdationDate { get; set; }
-
-        public int? UserDeletionId { get; set; }
-
-        public DateOnly? UserDeletionDate { get; set; }
-        public IFormFile? UploadedFile { get; set; }
         [ValidateNever]
-
         public virtual TrainingSector TrainigSector { get; set; } = null!;
     }
 }
--- TrainigSectorWebSite/Controllers/OpinionController.cs
+++ TrainigSectorWebSite/Controllers/OpinionController.cs
@@ -1,21 +1,66 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
+using TrainigSectorDataEntry.Interface;
+using TrainigSectorDataEntry.Models;
+using TrainigSectorDataEntry.ViewModel;
 
 namespace TrainigSectorWebSite.Controllers
 {
     public class OpinionController : Controller
     {
         IStringLocalizer<SharedResource> _localizer;
-        public OpinionController(IStringLocalizer<SharedResource> localizer)
+        private readonly IGenericService<ComplaintsAndSuggestion> _ComplaintsAndSuggestion;
+        public OpinionController(IGenericService<ComplaintsAndSuggestion> ComplaintsAndSuggestion, IStringLocalizer<SharedResource> localizer)
         {
+            _ComplaintsAndSuggestion = ComplaintsAndSuggestion;
             _localizer = localizer;
         }
+        [HttpGet]
         public IActionResult Index()
         {
             ViewData["Breadcrumb_MapPath"] = "معامل";
             ViewData["Breadcrumb_PageName"] = "Opinion";
             ViewData["Breadcrumb_ActivePage"] = "معامل هندسية";
             return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(ComplaintsAndSuggestionVM model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            // Save file
+            var folder = Path.Combine( "uploads/complaints");
+            Directory.CreateDirectory(folder);
+
+            var fileName = Guid.NewGuid() + Path.GetExtension(model.UploadedFile.FileName);
+            var path = Path.Combine(folder, fileName);
+
+            using var stream = new FileStream(path, FileMode.Create);
+            await model.UploadedFile.CopyToAsync(stream);
+
+            var entity = new ComplaintsAndSuggestion
+            {
+                Name = model.Name,
+                Telephone = model.Telephone,
+                Email = model.Email,
+                ComplaintText = model.ComplaintText,
+                FilePath = "/uploads/complaints/" + fileName,
+                IsActive = true,
+                TrainigSectorId=1,
+                UserCreationDate = DateOnly.FromDateTime(DateTime.Now)
+            };
+
+            await _ComplaintsAndSuggestion.AddAsync(entity);
+            //await _context.SaveChangesAsync();
+
+            TempData["Success"] = "تم إرسال رسالتك بنجاح";
+            return RedirectToAction("Index","Opinion");
         }
     }
 }
