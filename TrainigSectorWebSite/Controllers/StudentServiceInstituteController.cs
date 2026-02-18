069da0bef0fe3160f3169d375efaab08c4c4d4a3
Hossam <hosssam4009@gmail.com>
2026-02-18 13:56:26 +0200

    hossam

--- TrainigSectorWebSite/Controllers/StudentServiceInstituteController.cs
+++ TrainigSectorWebSite/Controllers/StudentServiceInstituteController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using TrainigSectorDataEntry.Interface;
+using TrainigSectorDataEntry.Logging;
+using TrainigSectorDataEntry.Models;
 
 namespace TrainigSectorWebSite.Controllers
 {
-    public class StudentServiceInstituteController : Controller
+    public class StudentServiceInstituteController : BaseController
     {
+       
+
+        IStringLocalizer<SharedResource> _localizer;
+        public StudentServiceInstituteController(IStringLocalizer<SharedResource> localizer)
+        {
+            _localizer = localizer;
+            
+        }
         public IActionResult Index()
         {
+            SetBreadcrumb(
+         mapPath: _localizer["MainPage"],
+         pageName: _localizer["StudentServices"],
+         activePage: _localizer["StudentServices"]
+);
             return View();
         }
     }
